cc66e87f1d1e733baa2307fafd16d9cf53619ae1
elletaro <166552032+elletaro@users.noreply.github.com>
2025-02-26 17:39:23 +0200

    add links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
     <div className="App">
       <h1>Weather React</h1>
       <Weather />
+      <p>
+        <a href="https://github.com/elletaro/my-app">Open source</a> code by
+        Chantelle Dlamini from <a href="https://www.shecodes.io/">SheCodes</a>
+      </p>
     </div>
   );
 }
